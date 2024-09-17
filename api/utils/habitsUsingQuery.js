105213caf42dd0adfd5d563e5b1e266e6dc7b5d0
Hnq <hancu345@gmail.com>
2024-09-17 12:06:17 +0300

    Habits based on days, all days habits

--- api/utils/habitsUsingQuery.js
+++ api/utils/habitsUsingQuery.js
@@ -1,7 +1,7 @@
 const getHabitsUsingQuery = (user, query) => {
   const day = Object.keys(query)[0];
   const habits = user.habits.filter((habit) => {
-    return habit.days.includes(day);
+    return habit.days.includes(day) || habit.days.includes("All Days");
   });
 
   return habits;
